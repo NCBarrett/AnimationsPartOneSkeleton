2e3cd7153cad2e6ffc61311d68ec13410030456d
Neill Barrett <barrettneill@gmail.com>
2022-01-06 13:13:27 -0800

    Initial commit

---
+++ app/src/main/java/com/example/bitamirshafiee/animationspartoneskeleton/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.bitamirshafiee.animationspartoneskeleton
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun constraintSetFirstExample(view : View){}
+    fun constraintSetSecondExample(view: View){}
+    fun placeHolderExample(view: View){}
+    fun circlePositioning(view: View){}
+}
---
+++ app/src/main/java/com/example/bitamirshafiee/animationspartoneskeleton/PlaceHolderExample.kt
@@ -0,0 +1,12 @@
+package com.example.bitamirshafiee.animationspartoneskeleton
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlaceHolderExample : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_place_holder_example)
+    }
+}
