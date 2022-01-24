3559ad9f5913becf80bf4fdda43c263c2abf4e5e
Neill Barrett <barrettneill@gmail.com>
2022-01-23 18:36:36 -0800

    Need to add databinding code for onClick

---
+++ app/src/main/java/com/example/bitamirshafiee/animationspartoneskeleton/FirstConstraintSet.kt
@@ -0,0 +1,44 @@
+package com.example.bitamirshafiee.animationspartoneskeleton
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.transition.TransitionManager
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
+
+class FirstConstraintSet : AppCompatActivity() {
+
+    private var showBigImage = false
+
+    private val constraintSetNormal = ConstraintSet()
+    private val constraintSetBig = ConstraintSet()
+
+    private var rootLayout : ConstraintLayout? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first_constraint_set)
+
+        rootLayout = findViewById(R.id.start_animation)
+
+        constraintSetNormal.clone(rootLayout!!)
+
+        constraintSetBig.load(this@FirstConstraintSet, R.layout.first_constraint_set_end)
+    }
+
+    fun toggledMode(view: android.view.View) {
+        TransitionManager.beginDelayedTransition(rootLayout!!)
+
+        showBigImage = !showBigImage
+
+        applyConfig()
+    }
+
+    private fun applyConfig() {
+        if (showBigImage) {
+            constraintSetBig.applyTo(rootLayout!!)
+        } else {
+            constraintSetNormal.applyTo(rootLayout!!)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/bitamirshafiee/animationspartoneskeleton/first_contraint_set_end.java
@@ -0,0 +1,6 @@
+package com.example.bitamirshafiee.animationspartoneskeleton;
+
+import android.app.Activity;
+
+public class first_contraint_set_end extends Activity {
+}
